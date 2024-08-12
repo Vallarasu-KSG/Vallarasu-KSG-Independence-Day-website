b846c1c8b71fbb79c97c1c93ab86f68468e6e417
Vallarasu-KSG <kvallarasu@2003.com>
2024-08-12 14:05:53 +0530

    Independence-Day-website

---
+++ t1.js
@@ -0,0 +1,55 @@
+function openPage(pageName,elmnt,color) 
+{
+    var i, tabcontent, tablinks;
+    tabcontent = document.getElementsByClassName("tabcontent");
+    for (i = 0; i < tabcontent.length; i++) {
+      tabcontent[i].style.display = "none";
+    }
+    tablinks = document.getElementsByClassName("tablink");
+    for (i = 0; i < tablinks.length; i++) {
+      tablinks[i].style.backgroundColor = "";
+    }
+    document.getElementById(pageName).style.display = "block";
+    // elmnt.style.backgroundColor = color;
+  }
+  
+  // Get the element with id="defaultOpen" and click on it
+  document.getElementById("defaultOpen").click();
+
+
+  // Navbar mobile
+let menu = document.querySelector('#menu-icon');
+let navlist = document.querySelector('.navlist');
+
+menu.onclick = () => {
+    menu.classList.toggle('bx-x');
+    navlist.classList.toggle('open');
+};
+
+const sr = ScrollReveal ({
+        distance:'65px',
+        duration: 2600,
+        delay: 450,
+        reset:true
+});
+
+sr.reveal('.hero-text',{delay:200, origin:'top'});
+sr.reveal('.hero-img',{delay:450, origin:'top'});
+sr.reveal('.icons',{delay:500, origin:'left'});
+sr.reveal('.scroll-down',{delay:500, origin:'right'});
+
+
+const Login2=document.querySelector(".Login2")
+
+//forget-password-script
+function fg()
+{
+    const a=prompt("Please enter the email")
+    if(a || null)
+    {
+        alert("Verification code sent to your email")
+    }else
+    {
+        alert("You not enter the email")
+    }
+}
